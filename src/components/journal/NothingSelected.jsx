202cdfcec07b120fbb935b91a2f7b2a776ea5469
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2021-01-11 12:47:22 -0600

    Component 'NothingSelected' was added

---
+++ src/components/journal/NothingSelected.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const NothingSelected = () => {
+    return(
+        <div className="nothing__main-content">
+           <p>Select some entry from your list.</p>
+           <br/>
+           <p>Or create a new entry</p>
+           <i className="far fa-star fa-4x mt-2"></i>
+        </div>
+    )
+}
