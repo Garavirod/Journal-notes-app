335876f54cf4cc8e611713f73653f34d262c7524
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2021-01-12 21:43:22 -0600

    readux and react-readux was added

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,26 @@
+/* 
+    {
+        uid: asjdiasjdkljalksdjkld
+        name: username
+    }
+*/
+
+import { types } from "../types/types";
+
+
+
+export const authReducer = (state ={}, action ) => {
+    switch (action.type) {
+        case types.login:
+            return{
+                uid: action.payload.uid, //from firebase
+                name:action.payload.displayName
+            }                        
+        case types.logout:
+            return{
+
+            }
+        default:
+            return state;
+    }
+}
