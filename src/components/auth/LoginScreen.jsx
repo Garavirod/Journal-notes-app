f7058aec968ba6db6e2c624c85eff950346e9d5b
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2021-01-13 14:41:42 -0600

    Dispatcher login was added

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -1,23 +1,54 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { login } from "../../actions/auth";
+import { useForm } from "../../hooks/useForm";
+import { useDispatch } from "react-redux";
 
 export const LoginScreen = () => {
+
+  /* redux dispathcer*/
+  const dispatch = useDispatch();
+
+
+  /* custom hook form */
+  const [formvalues, handleInputChange] = useForm(
+    {
+      email:'rga@gamil.com',
+      password:'asdfghj123',
+    }
+  );
+
+  const {
+    email,
+    password
+  } = formvalues;
+
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    dispatch(login(12345,'Rodrigo'));
+  };
+
   return (
     <React.Fragment>
       <h3 className="auth__title">Login</h3>
-      <form>
+      <form onSubmit={handleLogin}>
         <input
           className="auth__input"
           autoComplete="off"
           type="text"
           placeholder="email"
           name="email"
+          value={email}
+          onChange={handleInputChange}
         />
         <input
           className="auth__input"
           type="password"
           placeholder="Password"
           name="password"
+          value={password}
+          onChange={handleInputChange}
         />
         <button type="submit" className="btn btn-primary btn-block">login</button>
       </form>      
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -9,12 +9,12 @@ import { types } from "../types/types";
 
 
 
-export const authReducer = (state ={}, action ) => {
+export const authReducer = (state = {}, action ) => {
     switch (action.type) {
         case types.login:
             return{
                 uid: action.payload.uid, //from firebase
-                name:action.payload.displayName
+                name: action.payload.displayName
             }                        
         case types.logout:
             return{
