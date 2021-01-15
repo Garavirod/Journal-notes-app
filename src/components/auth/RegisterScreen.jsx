8f2345fdf6b13d11b0689481dc0c218b1b5f82aa
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2021-01-15 13:22:21 -0600

    useForm was added to Register

--- src/components/auth/RegisterScreen.jsx
+++ src/components/auth/RegisterScreen.jsx
@@ -1,17 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "../../hooks/useForm";
 
 export const RegisterScreen = () => {
+  const [values, handleInputChange] = useForm({
+    name:'Rodrgio',
+    email:'gara@gmail.com',
+    password1:'123456',
+    password2:'123456'
+  });
+
+  const {
+    name,
+    email,
+    password1,
+    password2
+  } = values;
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    console.log(values);
+
+  }
+
   return (
     <React.Fragment>
       <h3 className="auth__title">Register</h3>
-      <form>
+      <form onSubmit={handleRegister}>
       <input
           className="auth__input"
           autoComplete="off"
           type="text"
           placeholder="Name"
           name="name"
+          value={name}
+          onChange={handleInputChange}
         />
         <input
           className="auth__input"
@@ -19,19 +42,25 @@ export const RegisterScreen = () => {
           type="text"
           placeholder="email"
           name="email"
+          value={email}
+          onChange={handleInputChange}
         />
         <input
           className="auth__input"
           type="password"
           placeholder="Password"
-          name="password"
+          name="password1"
+          value={password1}
+          onChange={handleInputChange}
         />
 
         <input
           className="auth__input"
           type="password"
           placeholder="Confirm password"
-          name="psdConfirm"
+          name="password2"
+          value={password2}
+          onChange={handleInputChange}
         />
         <button type="submit" className="btn btn-primary btn-block mb-5">
           Register
