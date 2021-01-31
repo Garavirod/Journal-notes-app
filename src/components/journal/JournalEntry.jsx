9cae84a01e19819d0e3369ac6f891ed7ce03a15a
Garavirod <rodrigogarciaavila26@gmail.com>
2021-01-31 11:52:51 -0600

    Notes were added from firebase asn moment.js was used for dates fromat

--- src/components/journal/JournalEntry.jsx
+++ src/components/journal/JournalEntry.jsx
@@ -1,30 +1,37 @@
 import React from 'react';
+import moment from 'moment';
+export const JournalEntry= ({id, date, title, body, url}) => {
 
-export const JournalEntry= () => {
+    const noteDate = moment(date)
+
     return (
         <div className="journal__entry pointer">
-            <div 
-                className="journal__entry-picture"
-                style={{
-                    backgroundSize:'cover',
-                    objectFit:'contain',
-                    backgroundImage:'url(https://preview.redd.it/9g7hf0k9jz561.png?width=640&height=798&crop=smart&auto=webp&s=44249240939a799a1c0314b6b86772fbc2c46c3f)'
-                }}
-            >
-            </div>
+            {
+                url && 
+                <div 
+                    className="journal__entry-picture"
+                    style={{
+                        backgroundSize:'cover',
+                        objectFit:'contain',
+                        backgroundImage:`url(${url})`,
+/*                         backgroundImage:'url(https://preview.redd.it/9g7hf0k9jz561.png?width=640&height=798&crop=smart&auto=webp&s=44249240939a799a1c0314b6b86772fbc2c46c3f)'
+ */                    }}
+                >
+                </div>
+            }
 
             <div className="journal__entry-meta">
 
                 <div className="journal__entry-body">
-                    <p className="journal__entry-title">Do it or not, but never try to</p>
+                    <p className="journal__entry-title">{title}</p>
                     <p className="journal__entry-content">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus voluptate possimus soluta expedita ipsa consequatur quia nisi officiis quam! Maxime repellat officia temporibus ipsa magnam voluptatum, odio sequi aut velit!
+                        {body}
                     </p>
                 </div>
 
                 <div className="journal__entry-date-box">
-                    <span>Monday</span>
-                    <h4>23</h4>                
+                    <span>{noteDate.format('dddd')}</span>
+                    <h4>{noteDate.format('Do')}</h4>                
                 </div>
 
             </div>
